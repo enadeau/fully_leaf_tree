bb44137ba5b24fd5354cdb89744d69f4169f2f97
Emile Nadeau <nadeau.emile@gmail.com>
2017-07-30 14:16:01 -0400

    Improve undoing method and add many statistic.
    -Replace the subtree_stack wiht a user_intervention_stack that store all
    node modify by user
    -Add subtree_size, num_rejected statistics
    -Implement undoing of a rejection
    -Centralize the undoing in one method that call hidden mehtod for the
    appropriate cases

--- graph_border.py
+++ graph_border.py
@@ -2,12 +2,12 @@ class GraphBorder():
     r"""
     Object represent a induced subtree of a graph and the surrounding of this subtree.
     The data structure also catch up a bit of the evolution of the tree over time.
-    
+
     ATTRIBUTES:
         vertex_status: Dictionnary that store the state of each vertices with options.
         The status of a vertex v is of the form:
             - ("s",d) if v is in the subtree with degree d in the subtree;
-            - ("b",None) if v is not in the subtree and is adjacent to exactly one 
+            - ("b",None) if v is not in the subtree and is adjacent to exactly one
               vertex of the subtree;
             - ("r",i) if v rejected of the subtree. If i==v the vertex was voluntary rejected
               from a potential subtree by the users. Otherwise it indicates that the addition
@@ -16,13 +16,22 @@ class GraphBorder():
             - ("a",None) if v can eventually be add to the subtree but his addition will
               created a disconnected subgraph in the actual state (i.e v is not on the border
               of the subtree)
-        
+
         graph: The graph used to induced the subtree
 
-        leaf: The number of leaves of the subtree. Note that with this class a tree with only
-        one vertex is consider to have one leaf
+        num_leaf: The number of leaves of the subtree. Note that with this class a tree with only
+        one vertex is consider to have one leaf but the function subtree_num_leaf makes
+        the correction.
 
-        subtree_stack: Stack of the subtree vertices in the order they where added to the tree.
+        border_size: The number of vertices in the border
+
+        subtree_size: The number of vertices in the subtree
+
+        num_rejected: The number of vertices that are rejected
+
+        user_intervention_stack: Stack of the subtree vertices in the order of which the user
+        explicitly specify the state (by addition or rejection). Last vertex the user acts on
+        is on top.
     """
 
     def __init__(self, G):
@@ -32,8 +41,11 @@ class GraphBorder():
         """
         self.vertex_status=dict()
         self.graph=G
-        self.leaf=0
-        self.subtree_stack=[]
+        self.num_leaf=0
+        self.border_size=0
+        self.subtree_size=0
+        self.num_rejected=0
+        self.user_intervention_stack=[]
         for v in G.vertex_iterator():
             self.vertex_status[v]=("a", None)
 
@@ -43,12 +55,16 @@ class GraphBorder():
         Return any vertex if the subtree is empty.
         Return None if the current solution can't be extend.
         """
-        for v in self.vertex_status:
-            if self.vertex_status[v][0]=="b":
-                return v
-        for v in self.vertex_status:
-            if self.vertex_status[v][0]=="a":
-                return v
+        if self.subtree_size==0:
+            #The subtree is empty, any non rejected vertex can be add
+            for v in self.vertex_status:
+                if self.vertex_status[v][0]=="a":
+                    return v
+        else:
+            #The subtree is unempty, a vertex of the border must be add
+            for v in self.vertex_status:
+                if self.vertex_status[v][0]=="b":
+                    return v
         return None
 
     def add_to_subtree(self,v):
@@ -57,51 +73,59 @@ class GraphBorder():
         with a first vertex.
 
         INPUTS:
-            v - A vertex of the border or if the subtree is empty or any vertex if the
-                the subtree is empty.
+            v - A vertex of the border or if the subtree is empty or any unrejected vertex
+                if the the subtree is empty.
         """
         assert self.vertex_status[v][0]=="b" or ("b",None) not in self.vertex_status.values()
         for u in self.graph.neighbor_iterator(v):
             (state,info)=self.vertex_status[u]
             if state=="a":
                 self.vertex_status[u]=("b",None)
+                self.border_size+=1
             elif state=="s":
                 self.vertex_status[u]=(state,info+1)
                 if info==1:
-                    self.leaf-=1
+                    self.num_leaf-=1
             elif state=="b":
+                self.border_size-=1
+                self.num_rejected+=1
                 self.vertex_status[u]=("r",v)
             #If the vertices is already rejected we do nothing
         if self.vertex_status[v][0]=="b": #The vertex extend a current solution
             self.vertex_status[v]=("s",1)
         else: #The vertex is the first vertex to be set to "s"
             self.vertex_status[v]=("s",0)
-        self.subtree_stack.append(v)
-        self.leaf+=1
+        self.num_leaf+=1
+        self.subtree_size+=1
+        self.user_intervention_stack.append(v)
 
-    def remove_last_addition(self):
+    def _remove_last_addition(self,v):
         r"""
-        Removes the last inserted vertex to the subtree.
+        Removes the last inserted vertex v to the subtree.
         """
-        assert len(self.subtree_stack)>0
-        v=self.subtree_stack.pop()
-        for u in self.graph.neighbor_iterator(v): 
+        for u in self.graph.neighbor_iterator(v):
             (state,info)=self.vertex_status[u]
             #Impossible that state is available
             if state=="b":
                 self.vertex_status[u]=("a", None)
+                self.border_size-=1
             elif state=="s":
                 self.vertex_status[u]=(state,info-1)
                 if info==2:
-                    self.leaf+=1
+                    self.num_leaf+=1
             #At this point the state must be "r"
             elif info==v:
                 self.vertex_status[u]=("b", None)
-        if len(self.subtree_stack)>0:
+                self.num_rejected-=1
+                self.border_size+=1
+
+        self.subtree_size-=1
+        if self.subtree_size>0:
             self.vertex_status[v]=("b", None)
+            self.border_size+=1
         else: #We remove the last vertex from the subtree
             self.vertex_status[v]=("a",None)
-        self.leaf-=1
+        self.num_leaf-=1
 
     def reject_vertex(self,v):
         r"""
@@ -109,5 +133,38 @@ class GraphBorder():
         When a vertex v has been rejected using this method,
         his value in vertex_status is set to ("r",v)
         """
-        assert self.vertex_status[v][0]!="s"
+        assert self.vertex_status[v][0]=="b" or self.subtree_size==0
         self.vertex_status[v]=("r",v)
+        self.num_rejected+=1
+        self.user_intervention_stack.append(v)
+
+    def _unreject_last_manual_rejection(self,v):
+        r"""
+        Cancel the last manual rejection. Vertex v must be te last rejected vertex.
+        """
+        self.num_rejected-=1
+        if self.subtree_size==0:
+            self.vertex_status[v]=("a",None)
+        else:
+            self.vertex_status[v]=("b",None)
+            self.border_size+=1
+
+    def undo_last_user_action(self):
+        r"""
+        Undo the last user intervention which is either and addition to the subtree
+        or a rejection.
+        """
+        v=self.user_intervention_stack.pop()
+        if self.vertex_status[v][0]=="s":
+            self._remove_last_addition(v)
+        else:
+            self._unreject_last_manual_rejection(v)
+
+    def subtree_num_leaf(self):
+        r"""
+        Return the number of leaf in the subtree
+        """
+        if self.subtree_size==1:
+            return 0
+        else:
+            return self.num_leaf
