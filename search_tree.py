5e848c6550810d58a397394a13688957dde37ac7
Émile Nadeau <nadeau.emile@gmail.com>
2018-01-09 20:55:04 -0500

    New functionnality for research tree

--- search_tree.py
+++ search_tree.py
@@ -2,19 +2,57 @@ load('flis_configuration.py')
 
 class SearchTree(object):
 
-    def __init__(self, configuration):
+    def __init__(self, configuration, method='std'):
+        assert method in ['std', 'stab', 'stab2']
         self.root = configuration
         v = self.root.vertex_to_add()
         if v != None:
+            #Inclusion side
             self.left = deepcopy(configuration)
             self.left.include_vertex(v)
-            self.left = SearchTree(self.left)
+            if method == 'stab2':
+                manual_exclude = [u for u in self.left.graph if \
+                        self.left.vertex_status[u] == (self.left.EXCLUDED, u)]
+                A = self.left.graph.automorphism_group()
+                S = A.stabilizer(self.left.subtree_vertices, action='OnSets')
+                #The assert must be commented in isometric_extension
+                for u in manual_exclude:
+                    for w in S.orbit(u):
+                        if self.left.vertex_status[w][0] == self.left.BORDER:
+                            self.left.exclude_vertex(w)
+            self.left = SearchTree(self.left, method=method)
+            #Exclusion side
             self.right = deepcopy(configuration)
-            self.right.exclude_vertex(v)
-            self.right = SearchTree(self.right)
+            if method == 'std':
+                self.right.exclude_vertex(v)
+            elif method in ['stab', 'stab2']:
+                for u in self.right.isometric_extension(v):
+                    self.right.exclude_vertex(u)
+            self.right = SearchTree(self.right, method=method)
         else:
             self.left = None
             self.right = None
+
+    def size(self):
+        r"""
+        Return the number of configuration in the search tree
+        """
+        if self.left != None:
+            return 1 + self.left.size() + self.right.size()
+        else:
+            return 1
+
+    def subtree_iterator(self):
+        r"""
+        Iterate among all subtree in the leaves of the search tree
+        """
+        if self.left == None:
+            yield self.root.subtree_vertices
+        else:
+            for a in self.left.subtree_iterator():
+                yield a
+            for a in self.right.subtree_iterator():
+                yield a
 
     def digraph(self):
         if self.left != None:
@@ -37,3 +75,17 @@ class SearchTree(object):
         elif node_labels == 'stab_card':
             raise NotImplementedError
 
+    def test_property(self, test):
+        r"""
+        Test the property verified by the function test on all configuration
+        of the search
+
+        INPUT:
+
+       - ``test``: A boolean function taking a configuraiton as parameter
+        """
+        if self.left != None:
+            return test(self.root) and self.left.test_property(test) and \
+                    self.right.test_property(test)
+        else:
+            return test(self.root)
